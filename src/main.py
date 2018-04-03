779f59087cd423769c089e749bf25fb2d71c6ef2
= <john@growth.gr>
2018-04-04 00:29:31 +0300

    use grayscale on images

--- src/main.py
+++ src/main.py
@@ -6,11 +6,17 @@ Example usage of wordcloud
 Generating the word cloud for the small dataset
 """
 from wordcloud import WordCloud
+import random
+
 from Categories import Categories
 
 # CONSTANTS
 TRAIN_SET_FILE = '../datasets/small_train_set.csv'
 IMAGE_FOLDER = '../output/images/'
+
+def grey_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
 
 # FUNCTIONS
 def generate_word_cloud():
@@ -20,6 +26,8 @@ def generate_word_cloud():
   for category_name in categories.categories:
     text = categories.get_body_as_string(category_name)
     wordcloud = WordCloud().generate(text)
+    default_colors = wordcloud.to_array()
+    wordcloud.recolor(color_func=grey_color_func, random_state=3)
 
     # display the image
     image = wordcloud.to_file(IMAGE_FOLDER + category_name + '.png')
