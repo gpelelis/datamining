e601b4b9ea1cd5cf373632a2c6c91ded024bec7b
= <john@growth.gr>
2018-04-03 22:49:14 +0300

    initial code to test wordcloud

---
+++ readme.md
@@ -0,0 +1,21 @@
+### wordcloud
+Here you can find the [documentation of wordcloud API](http://amueller.github.io/word_cloud/generated/wordcloud.WordCloud.html#wordcloud.WordCloud)   
+
+**usage example:**
+```python
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+TRAIN_SET_FILE = '../datasets/test_wordcloud.txt'
+text = open(TRAIN_SET_FILE).read()
+wordcloud = WordCloud().generate(text)
+
+# display the image
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.show()
+```
+
+**tips**: 
+to make it work you may need to run 
+- ```conda install -c matplotlib ```
+- on macos run the app as pythonw (not python)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-print("hello world")
+#!/usr/bin/env python
+"""
+Example usage of wordcloud
+===============
+
+Generating the word cloud for the small dataset
+"""
+
+from os import path
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+TRAIN_SET_FILE = '../datasets/test_wordcloud.txt'
+text = open(TRAIN_SET_FILE).read()
+wordcloud = WordCloud().generate(text)
+
+# display the image
+plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+
+
+# # lower max_font_size
+# wordcloud = WordCloud(max_font_size=40).generate(text)
+# plt.figure()
+# plt.imshow(wordcloud, interpolation="bilinear")
+# plt.axis("off")
+plt.show()
